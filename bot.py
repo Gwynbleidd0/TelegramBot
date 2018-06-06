48754e63dfd74563d4bc9a32351b909a763e303d
root <root@localhost.localdomain>
2018-06-06 18:21:44 +0000

    Nothing

---
+++ bot.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import config
+import telebot
+bot = telebot.TeleBot(config.token)
+@bot.message_handler(content_types=["text"])
+def repeating_messages(message):
+	bot.send_message(message.chat.id, message.text)
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
