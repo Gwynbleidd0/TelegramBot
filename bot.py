36b46206b731d8e764a169a8726e40b98ebceed5
root <root@localhost.localdomain>
2018-06-07 12:54:54 +0000

    Nothing

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
+from telebot import apihelper
+apihelper.proxy = {'https':'https://79.120.177.106:8080'}
 bot = telebot.TeleBot(config.token)
 @bot.message_handler(content_types=["text"])
 def repeating_messages(message):
 	bot.send_message(message.chat.id, message.text)
 if __name__ == '__main__':
-	bot.polling(none_stop=True)
+		bot.polling(none_stop=True)
