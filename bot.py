9bd456828f2ff52007c08a2ebc58b429a45133ea
root <root@localhost.localdomain>
2018-06-07 22:36:11 +0000

    fix0

--- bot.py
+++ bot.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 import config
 import telebot
+import datetime
 from telebot import apihelper
-apihelper.proxy = {'https':'https://79.120.177.106:8080'}
+from telebot import types
+apihelper.proxy = {'https':'https://35.199.64.211:80'}
+
 bot = telebot.TeleBot(config.token)
+#def datefun():
+#	now = datetime.datetime.now
+#	time1now = datetime.datetime.time()
+#	return time1now
+
 @bot.message_handler(content_types=["text"])
 def repeating_messages(message):
-	bot.send_message(message.chat.id, message.text)
+	if message.text=="/help":
+		bot.send_message(message.chat.id,"List of commands")
+	if message.text=="/data":
+		bot.send_message(message.chat.id,datetime.datetime.today().strftime("%d/%m/%y %H:%M"))
 if __name__ == '__main__':
 		bot.polling(none_stop=True)
